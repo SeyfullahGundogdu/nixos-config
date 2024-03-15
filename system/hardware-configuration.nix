ab09292dbe0dad3ef972b22c3c77654597c32bf7
Seyfullah Gündoğdu <seyfullahgundogdu74@gmail.com>
2024-03-16 00:52:37 +0300

    Removed unnecessary boot option.

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -16,10 +16,6 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
-  boot.kernelParams = [
-    "i915.enable_fbc=1"
-    #"i915.enable_psr=2"
-  ];
   #NVIDIA stuff
   hardware.opengl = {
     enable = true;
