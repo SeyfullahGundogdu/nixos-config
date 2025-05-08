78e59ab52b1bf3c8ab325de2bc591bbd26748c3c
Seyfullah Gundogdu <seyfullahgundogdu74@gmail.com>
2025-05-08 19:22:15 +0300

    rm mako

--- home/bake/programs/hyprland/hypr/walp.sh
+++ home/bake/programs/hyprland/hypr/walp.sh
@@ -2,8 +2,8 @@ wallpaper_folder="${HOME}/Pictures/walls/aesthic-wallpapers"
 selected_file=$(shuf -n 1 -e "$wallpaper_folder"/*)
 wall="$selected_file"
 wal -i "$wall";
-pkill mako;
+#pkill mako;
 pkill waybar;
-mako &
+#mako &
 waybar &
 swww img --transition-type grow --transition-pos 0.5,0.5 --transition-fps 60 $wall;
--- home/bake/programs/pywal/default.nix
+++ home/bake/programs/pywal/default.nix
@@ -2,7 +2,7 @@
   programs.pywal.enable = true;
   home.file = {
     # pywal template for mako compatible config
-    ".config/wal/templates/colors-mako".source = ./colors-mako;
+    # ".config/wal/templates/colors-mako".source = ./colors-mako;
     # same config for hyprland and hyprlock
     ".config/wal/templates/colors-hyprland.conf".source = ./colors-hyprland.conf;
   };
